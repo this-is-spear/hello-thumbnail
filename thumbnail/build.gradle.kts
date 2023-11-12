5d114fae1342a04bdc6a4ef8937cb3f9316f9607
이건창 <92219795+this-is-spear@users.noreply.github.com>
2023-11-12 23:53:36 +0900

    chore : 코루틴, aws sdk 추가

--- thumbnail/build.gradle.kts
+++ thumbnail/build.gradle.kts
@@ -15,6 +15,15 @@ dependencies {
     if (osdetector.arch.equals("aarch_64")) {
         implementation("io.netty:netty-resolver-dns-native-macos:4.1.79.Final:osx-aarch_64")
     }
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    /*
+    aws.sdk.kotlin:s3:0.34.5는 베타 버전입니다.
+    첨고 자료 : https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/get-started.html
+    참고 자료 : https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/kotlin/services/s3/Readme.md
+     */
+    implementation("aws.sdk.kotlin:s3:0.34.5-beta")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")

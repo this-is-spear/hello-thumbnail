b7751a8c0cae883d99b67fd0fd74c01124ed7561
이건창 <92219795+this-is-spear@users.noreply.github.com>
2023-11-13 23:14:12 +0900

    refactor : 임포트라인 정리

--- thumbnail/src/main/kotlin/tis/thumbnail/ThumbnailController.kt
+++ thumbnail/src/main/kotlin/tis/thumbnail/ThumbnailController.kt
@@ -1,7 +1,6 @@
 package tis.thumbnail;
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
